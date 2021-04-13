9dd6a6ea29177c0e6c4eac233e4ce00c4d77408b
ejrossma <ejrossma@ucsc.edu>
2021-04-13 03:53:15 -0700

    Fixed All the Errors

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.isFiring = false; //track firing status
         this.moveSpeed = 2; //pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket'); //add rocket sfx
     }
 
     update() {
@@ -19,8 +20,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         //fire button
-        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play(); //play sound
         }
         // if fired, move rocket up
         if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
